580bb462a74249b32a7abb6be30b0fe2ab0ac4e1
Backspac3_N00B <173277005+ImOutOfBounds@users.noreply.github.com>
2025-08-02 12:25:36 -0300

    camera adjsustments

--- lost-media/scripts/player.gd
+++ lost-media/scripts/player.gd
@@ -1,9 +1,14 @@
 extends CharacterBody3D
 
-
-const SPEED = 5.0
+var speed
+const WALK_SPEED = 5.0
+const RUN_SPEED = 9.0
 const JUMP_VELOCITY = 4.5
 const SENSITIVITY = .002
+
+const BOB_FREQ = 2.0
+const BOB_AMP = 0.08
+var t_bob = 0.0
 
 @onready var head = $Head
 @onready var camera = $Head/Camera3D
@@ -16,11 +21,22 @@ func _unhandled_input(event: InputEvent) -> void:
 		head.rotate_y(-event.relative.x * SENSITIVITY)
 		camera.rotate_x(-event.relative.y * SENSITIVITY)
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
+
+func headBob(bob):
+	var pos = Vector3.ZERO
+	pos.y = sin(bob * BOB_FREQ) * BOB_AMP
+	pos.x= sin(bob * BOB_FREQ / 2) * BOB_AMP
+	return pos
 	
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		
+	if Input.is_action_pressed("shift"):
+		speed = RUN_SPEED
+	else:
+		speed = WALK_SPEED
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -30,11 +46,19 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+	
+	if is_on_floor():
+		if direction:
+			velocity.x = direction.x * speed
+			velocity.z = direction.z * speed
+		else:
+			velocity.x = move_toward(velocity.x, 0, speed)
+			velocity.z = move_toward(velocity.z, 0, speed)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = lerp(velocity.x, direction.x * speed, delta * 2.0)
+		velocity.z = lerp(velocity.z, direction.z * speed, delta * 2.0)
+		
+	t_bob += delta * velocity.length() * float(is_on_floor())
+	camera.transform.origin = headBob(t_bob)
 
 	move_and_slide()
